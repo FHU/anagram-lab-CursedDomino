c61d2f70667b085e0225a4f00eaa60df57308fdf
CursedDomino <dillon.daughrity@outlook.com>
2024-02-06 09:59:30 -0600

    Test

--- main.py
+++ main.py
@@ -1,8 +1,31 @@
-#REMOVE PASS AND FIX THIS FUNCTION
 def anagram():
-    pass
+    word1 = input()
+    word2 = input()
+
+    if word1.isspace == True:
+        return False
+    
+    word1 = word1.replace(' ', '')
+    word2 = word2.replace(' ', '')
+
+    word1 = word1.lower()
+    word2 = word2.lower()
+
+    if len(word1) == len(word2):
+        sum1 = 0
+        sum2 = 0
+        for letter in word1:
+            sum1 += word1.count(letter)
+        for letter in word2:
+            sum2 += word2.count(letter)
+            
+        if sum1 == sum2:
+            return True
+        else:
+            return False
+    else:
+        return False
+
 
 if __name__ == '__main__':
-    #REMOVE PASS YOUR CODE GOES HERE
-    pass
-    
+    print(anagram())
